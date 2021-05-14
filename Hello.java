5659714c1913f8769683a944d55699bfb3d3510d
braxus32 <aguirre@plu.edu>
2021-05-14 00:58:37 -0700

    Updated Hello.java again

--- Hello.java
+++ Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("Hello");
         System.out.println("Goodbye");
         System.out.println("Hello again");
+        System.out.println("Goodbye again");
     }
 }
